dba34b1d55074a67bed4e72d2fbe345fc248c368
cifucarst <cifentesparraandres@gmail.com>
2023-12-20 09:04:44 -0500

    organizing the content structure

---
+++ oop/1-first-video/4-script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+class Rectangulo():
+    def __init__(self,ancho,alto) -> None:
+        self.ancho = ancho
+        self.alto = alto
+
+    @property
+    def area(self):
+        return self.ancho * self.alto
+    
+    def __str__(self) -> str:
+        pass
+
+rect1 = Rectangulo(20,80)
+print(f"\n[+] El area es {rect1.area}")
+print(rect1)
