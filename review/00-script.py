0003db5523a4605842a2ec34238f204283026d8d
cifucarst <cifentesparraandres@gmail.com>
2024-05-10 20:34:05 -0500

    review of comments, variables, constants, primitive types

---
+++ review/00-script.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# https://python.org
+
+# Single-line comment
+
+"""
+This is also
+a multi-line comment
+"""
+
+'''
+This is also
+a multi-line comment
+'''
+
+my_variable = "My variable"
+my_variable = "New value of my variable"
+
+MY_CONSTANT = "My constant"  # by convention
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "My string"
+my_other_string = 'My other string'
+
+print("Hello, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
